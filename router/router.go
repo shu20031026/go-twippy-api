f3ad238265d1b92297fd6d18f5d8ffdc5014c68d
shu20031026 <shu20031026@gmail.com>
2022-05-29 15:10:35 +0900

    fix: go.mod

--- router/router.go
+++ router/router.go
@@ -1,20 +1,18 @@
 package router
 
 import (
-	"fmt"
 	"main/controller"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
+	// }
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
