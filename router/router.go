fe24e0b1558c1105ecf1086fce76a74b009b4ec8
shu20031026 <shu20031026@gmail.com>
2022-05-29 14:16:19 +0900

    fix:PORTを環境変数化

--- router/router.go
+++ router/router.go
@@ -1,13 +1,20 @@
 package router
 
 import (
+	"fmt"
 	"main/controller"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
+	}
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -15,5 +22,5 @@ func Init() {
 	e.GET("/", controller.GetEndPoint)
 	e.GET("/tweets", controller.GetTweets)
 
-	e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
