9caeddd1f3ad9ecd225ef40c63485b8009f2fd22
shu20031026 <shu20031026@gmail.com>
2022-05-29 15:01:30 +0900

    fix

--- model/lordEnv.go
+++ model/lordEnv.go
@@ -1,14 +1,17 @@
 package model
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func LoadEnv() (string, string, string, string) {
-	// // err := godotenv.Load(".env")
-	// if err != nil {
-	// 	fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
-	// }
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
+	}
 	consumerKey := os.Getenv("CONSUMER_KEY")
 	consumerSecret := os.Getenv("CONSUMER_SECRET")
 	accessToken := os.Getenv("ACCESS_TOKEN")
