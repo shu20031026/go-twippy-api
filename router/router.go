72352be3579236c585cd9a0976733e2784a0a3fb
shu20031026 <shu20031026@gmail.com>
2022-05-29 15:49:59 +0900

    fix: ミドルウェアの削除

--- router/router.go
+++ router/router.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() {
@@ -14,12 +13,10 @@ func Init() {
 	// 	fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
 	// }
 	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
 	e.GET("/", controller.GetEndPoint)
 	e.GET("/tweets", controller.GetTweets)
 
-	e.Start(":" + os.Getenv("PORT"))
-	// e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+	// e.Start(":" + os.Getenv("PORT"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
