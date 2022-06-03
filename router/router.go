33fcce5020572e1bccb6f873c7f3cc0f6a101540
shu20031026 <shu20031026@gmail.com>
2022-06-03 23:54:57 +0900

    fix: PORT番号の有無による分岐

--- router/router.go
+++ router/router.go
@@ -1,22 +1,29 @@
 package router
 
 import (
+	"fmt"
 	"main/controller"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func Init() {
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
-	// }
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf(".envファイルの読み込みに失敗しました: %v", err)
+	}
+	var PORT string
+	if os.Getenv("PORT") == "" {
+		PORT = "80"
+	} else {
+		PORT = os.Getenv("PORT")
+	}
 	e := echo.New()
 
 	e.GET("/", controller.GetEndPoint)
 	e.GET("/tweets", controller.GetTweets)
 
-	// e.Start(":" + os.Getenv("PORT"))
-	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+	e.Logger.Fatal(e.Start(":" + PORT))
 }
